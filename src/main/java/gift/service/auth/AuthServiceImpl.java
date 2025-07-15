32888b64bbb0d1e8fcca64155b23615fdbece368
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-15 15:07:40 +0900

    fix: AuthService 에러 수정

--- src/main/java/gift/service/auth/AuthServiceImpl.java
+++ src/main/java/gift/service/auth/AuthServiceImpl.java
@@ -12,17 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthServiceImpl implements AuthService {
     
-    private final MemberRepository memberRepository;
+    private final MemberRepositoryJpa memberRepositoryJpa;
     private final JwtProvider jwtProvider;
     
-    public AuthServiceImpl(MemberRepository memberRepository, JwtProvider jwtProvider) {
-        this.memberRepository = memberRepository;
+    public AuthServiceImpl(MemberRepositoryJpa memberRepositoryJpa, JwtProvider jwtProvider) {
+        this.memberRepositoryJpa = memberRepositoryJpa;
         this.jwtProvider = jwtProvider;
     }
     
     @Override
     public MemberResponseDto login(LoginRequestDto requestDto) {
-        Member member = memberRepository.findMemberByEmail(requestDto.email());
+        Member member = memberRepositoryJpa.findByEmail(requestDto.email())
+            .orElseThrow(WrongIdOrPasswordException::new);
         
         if(!member.getPassword().equals(requestDto.password())) {
             throw new WrongIdOrPasswordException();
