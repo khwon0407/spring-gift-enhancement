f26af53aa588a5119e5601217ad449b71639a116
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-15 16:28:21 +0900

    feat : Jpa Test code 작성

---
+++ src/test/java/gift/repository/member/MemberRepositoryJpaTest.java
@@ -0,0 +1,40 @@
+package gift.repository.member;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import gift.entity.Member;
+import gift.entity.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MemberRepositoryJpaTest {
+    @Autowired
+    private MemberRepositoryJpa memberRepositoryJpa;
+    
+    @Test
+    void 저장() {
+        Member member = new Member(null, "email@email.com", "pwpw", Role.USER);
+        
+        var actual = memberRepositoryJpa.save(member);
+        
+        assertAll(
+            () -> assertThat(actual.getId()).isNotNull(),
+            () -> assertThat(actual.getEmail()).isEqualTo(member.getEmail())
+        );
+    }
+    
+    @Test
+    void 검색() {
+        Member member = new Member(null, "email@email.com", "pwpw", Role.USER);
+        memberRepositoryJpa.save(member);
+        
+        var actual = memberRepositoryJpa.findByEmail(member.getEmail()).get().getEmail();
+        
+        assertThat(actual).isEqualTo(member.getEmail());
+    }
+}
---
+++ src/test/java/gift/repository/product/ProductRepositoryJpaTest.java
@@ -0,0 +1,40 @@
+package gift.repository.product;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import gift.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ProductRepositoryJpaTest {
+    @Autowired
+    private ProductRepositoryJpa productRepositoryJpa;
+    
+    @Test
+    void 저장() {
+        Product product = new Product(null, "이름", 3000L, "None");
+        
+        var actual = productRepositoryJpa.save(product);
+        
+        assertAll(
+            () -> assertThat(actual.getId()).isNotNull(),
+            () -> assertThat(actual.getName()).isEqualTo(product.getName())
+        );
+    }
+    
+    @Test
+    void 검색() {
+        Product product = new Product(null, "이름", 3000L, "None");
+        
+        var temp = productRepositoryJpa.save(product);
+        
+        var actual = productRepositoryJpa.findById(temp.getId()).get().getName();
+        
+        assertThat(actual).isEqualTo(product.getName());
+    }
+}
