241a463ff54beb6be5e913372e036dafb0e47f34
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-15 13:49:26 +0900

    fix: product repository 선언 추가

--- src/main/java/gift/repository/product/ProductRepositoryJpa.java
+++ src/main/java/gift/repository/product/ProductRepositoryJpa.java
@@ -2,7 +2,9 @@ package gift.repository.product;
 
 import gift.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepositoryJpa extends JpaRepository<Product, Long> {
 
 }
