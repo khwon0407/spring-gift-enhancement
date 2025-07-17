5e519d0540971641addcb6b728e19709c51a8247
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-17 15:46:26 +0900

    fix: 예외 메시지 수정

--- src/main/java/gift/exception/badrequest/WrongPriceException.java
+++ src/main/java/gift/exception/badrequest/WrongPriceException.java
@@ -4,6 +4,6 @@ import gift.exception.common.BadRequestException;
 
 public class WrongPriceException extends BadRequestException {
     public WrongPriceException() {
-        super("가격은 0보다 커야 합니다.");
+        super("가격은 0원 이상이어야 합니다.");
     }
 }
