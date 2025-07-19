692098f576f8044f848506eed358ec4a6aae2bb5
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-19 14:42:36 +0900

    feat: Option Entity 추가

---
+++ src/main/java/gift/entity/Option.java
@@ -0,0 +1,58 @@
+package gift.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "options")
+public class Option {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "name", nullable = false)
+    private String name;
+    
+    @Column(name = "quantity", nullable = false)
+    private Long quantity;
+    
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false, foreignKey = @ForeignKey(name = "fk_option_product"))
+    private Product product;
+    
+    public Option() {
+    
+    }
+    
+    public Option(Long id, String name, Long quantity, Product product) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.product = product;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Long getQuantity() {
+        return quantity;
+    }
+    
+    public Product getProduct() {
+        return product;
+    }
+}
