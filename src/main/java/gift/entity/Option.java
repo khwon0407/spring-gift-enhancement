322d7c17d62f91dfdc9570a6688f358489b39588
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-19 14:50:55 +0900

    fix: Name unique 설정

--- src/main/java/gift/entity/Option.java
+++ src/main/java/gift/entity/Option.java
@@ -19,7 +19,7 @@ public class Option {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
     
     @Column(name = "quantity", nullable = false)
