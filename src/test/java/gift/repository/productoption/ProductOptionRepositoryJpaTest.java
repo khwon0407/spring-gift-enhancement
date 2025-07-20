d6a03dfce5f28d24e6173de6e1bd2d2b93fbcd51
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-20 17:50:11 +0900

    feat: Test code 작성

---
+++ src/test/java/gift/repository/productoption/ProductOptionRepositoryJpaTest.java
@@ -0,0 +1,43 @@
+package gift.repository.productoption;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import gift.entity.Product;
+import gift.entity.ProductOption;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ProductOptionRepositoryJpaTest {
+    @Autowired
+    ProductOptionRepositoryJpa productOptionRepositoryJpa;
+    
+    @Test
+    void 저장() {
+        Product product = new Product(1L, "아메리카노", 4500L, "http://image.url.americano");
+        ProductOption productOption = new ProductOption(null, "이름", 3000L, product);
+        
+        var actual = productOptionRepositoryJpa.save(productOption);
+        
+        assertAll(
+            () -> assertThat(actual.getId()).isNotNull(),
+            () -> assertThat(actual.getName()).isEqualTo(productOption.getName())
+        );
+    }
+    
+    @Test
+    void 검색() {
+        Product product = new Product(1L, "아메리카노", 4500L, "http://image.url.americano");
+        ProductOption productOption = new ProductOption(null, "이름", 3000L, product);
+        
+        var temp = productOptionRepositoryJpa.save(productOption);
+        
+        var actual = productOptionRepositoryJpa.findById(temp.getId()).get().getName();
+        
+        assertThat(actual).isEqualTo(productOption.getName());
+    }
+}
